26a51c5a7fa1b0e1f0ddb1613247b6e98b18cc5e
Henrique Alves <henriqueasemmer@gmail.com>
2023-09-27 22:58:35 -0300

    Initial Commit

---
+++ PokedexBackup/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PokedexBackup
+//
+//  Created by Henrique Semmer on 27/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PokedexBackup/PokedexBackupApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexBackupApp.swift
+//  PokedexBackup
+//
+//  Created by Henrique Semmer on 27/09/23.
+//
+
+import SwiftUI
+
+@main
+struct PokedexBackupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
